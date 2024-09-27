58003348ecf2affb8765a80ecb33f213528af3ba
harshapawar <harsha05pawar@gmail.com>
2024-09-27 09:54:27 +0530

    Number Match.py

---
+++ Number Match.py
@@ -0,0 +1,18 @@
+from collections import Counter
+def numberMatch(n,arr):
+    counts=Counter(arr)
+    a,b=0,0
+    for num,c in counts.items():
+        if num%2==0:
+            if c%2==0:
+                a+=1
+            else:
+                b+=1
+        else:
+            if c%2==0:
+                b+=1
+            else:
+                a+=1
+    if a>b:
+        print("A",a-b)
+    elif a
